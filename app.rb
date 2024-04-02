aeedcdbcca192191b3f58f8f1d7ba81d70f41a5f
IshowCarlBrooker <carl.brooker@elev.ga.ntig.se>
2024-04-02 23:26:21 +0200

    nypushhh

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require 'sinatra/flash'
 
 enable :sessions
 
@@ -10,7 +12,30 @@ get('/') do
 end
 
 get('/squad') do
+  db = SQLite3::Database.new("db/oversikt.db")
+  db.results_as_hash = true
+  @players = db.execute("SELECT * FROM playerstable")
   slim(:squadbuilder)
+end
+
+post('/squad') do
+  db = SQLite3::Database.new("db/oversikt.db")
+  db.results_as_hash = true 
+  team_name = params[:team_name]
+  if db.execute("SELECT id FROM teams WHERE team_name = ?", team_name) == []
+    db.execute("INSERT INTO teams (team_name) VALUES (?)", team_name)
+  end
+
+  i = 1
+  while i <= 11 do
+    player = params[:"playerr#{i}"]
+    team_id = db.execute("SELECT id FROM teams WHERE team_name = ?", team_name).first["id"]
+    player_id = db.execute("SELECT playerid FROM playerstable WHERE player_name = ?", player).first["playerid"]
+    db.execute("INSERT INTO player_team_rel (player_id, team_id) VALUES (?,?)", player_id, team_id)
+    i += 1
+  end
+
+  redirect('/huvudsida')
 end
 
 get('/playersss') do
@@ -22,7 +47,7 @@ post('/playersss') do
   player_position = params[:position]
   db = SQLite3::Database.new("db/oversikt.db")
   db.execute("INSERT INTO playerstable ( player_name, player_position) VALUES (?,?)", player_name, player_position)
-  redirect('/huvudsida')
+  redirect('/playersss')
 end
 
 get('/everyplayer') do
@@ -30,6 +55,13 @@ get('/everyplayer') do
   db.results_as_hash = true
   result = db.execute("SELECT * FROM playerstable")
   slim(:everyplayer, locals:{players:result})
+end
+
+get('/everysquad') do 
+  db = SQLite3::Database.new("db/oversikt.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM teams")
+  slim(:everysquad, locals:{squad:result})
 end
 
 get('/showlogin') do
@@ -46,14 +78,22 @@ post('/login') do
   db = SQLite3::Database.new('db/oversikt.db')
   db.results_as_hash = true
   result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-  pwdigest = result["pwdigest"]
-  id = result["id"]
 
-  if BCrypt::Password.new(pwdigest) == password
+
+  if result.nil?
+    flash[:notice] =  "Användare finns inte"
+    redirect('/showlogin')
+  else  
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+  end
+
+  if BCrypt::Password.new(pwdigest) == password 
     session[:id] = id
     redirect('/huvudsida')
-  else
-    "FEL LÖSENORD"
+  else 
+    flash[:notice] = "Fel Lösenord"
+    redirect('/showlogin')
   end
 end
 
@@ -67,9 +107,11 @@ post('/users/new') do
     password_digest = BCrypt::Password.create(password)
     db = SQLite3::Database.new('db/oversikt.db')
     db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
-    redirect('/')
+    flash[:notice] = "Användare skapad"
+    redirect('/showlogin')
   else 
-    "Fel Lösenord"
+    flash[:notice] = "Lösenorden matchar inte"
+    redirect('/')
   end
 end
 
